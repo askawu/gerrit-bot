302db29f8fb29a141b57a1428d4dc1cb7f413e12
Buildbot <builder@vanadium>
2014-09-04 19:29:24 +0800

    Add redmine and gerrit integration
    If the subject of the patch contains [t1234], it will update the
    redmine issue 1234 automatically.
    
    Maybe we need to rename this project to gerrit-bot since we're not
    simply doing git and hipchat integration.

--- hipchat-git-bot.py
+++ hipchat-git-bot.py
@@ -4,14 +4,17 @@ from twisted.internet import defer
 from twisted.internet import protocol
 import simplejson as json
 import os
+import re
 import subprocess
 
 GERRIT=""
 PROJECT_WHITE_LIST=[]
-HIPCHAT_CLI_SCRIPT=""
+HIPCHAT_CLI_SCRIPT="./hipchat_room_message"
 HIPCHAT_AUTH_TOKEN=""
 HIPCHAT_ROOM_ID=""
 BOT_NAME="GIT"
+REDMINE_CLI_SCRIPT="./redmine_update_issue"
+REDMINE_API_KEY=""
 
 class HookProtocol(protocol.ProcessProtocol):
     def outReceived(self, data):
@@ -21,13 +24,13 @@ class HookProtocol(protocol.ProcessProtocol):
         for line in lines:
             print "gerrit: %s" % line
             self.lineReceived(line)
-        
+
     def lineReceived(self, line):
         try:
             event = json.loads(line.decode('utf-8'))
         except ValueError:
             defer.succeed(None)
-       
+
         if not(type(event) == type({}) and "type" in event):
             log.msg("no type in event %s" % (line,))
             return defer.succeed(None)
@@ -47,7 +50,18 @@ class HookProtocol(protocol.ProcessProtocol):
                 msg += "gitweb:  <a href=\"http://%s/gitweb/?p=%s.git;a=commitdiff;h=%s\">link</a>\n" % (GERRIT, event["change"]["project"], event["patchSet"]["revision"])
                 p1 = subprocess.Popen(["echo", msg], stdout=subprocess.PIPE)
                 p2 = subprocess.Popen([HIPCHAT_CLI_SCRIPT, "-c", "green", "-t", HIPCHAT_AUTH_TOKEN, "-r", HIPCHAT_ROOM_ID, "-f", BOT_NAME], stdin=p1.stdout, stdout=subprocess.PIPE)
-                p2.communicate() 
+                p2.communicate()
+        elif event["type"] == "patchset-created":
+            pattern = re.compile(".*\[t(\d+)\].*")
+            match = pattern.match(event["change"]["subject"])
+            if match:
+                cmd = [REDMINE_CLI_SCRIPT,
+                       event["change"]["owner"]["name"],
+                       event["change"]["project"],
+                       event["change"]["url"],
+                       match.group(1),
+                       REDMINE_API_KEY]
+                ret = subprocess.call(cmd)
 
 hp = HookProtocol()
 ctrl = reactor.spawnProcess(hp, "ssh", args= ["ssh", "-p", "29418", GERRIT, "gerrit", "stream-events"])
