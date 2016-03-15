202573f62ec8b48804588bfb0e571dc3b666f15b
Buildbot <builder@vanadium>
2016-03-15 13:20:46 +0800

    Add acer/apps/CloudPBX in mickey project whitelist

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ from gerrit_bot import GerritBot
 MINNIE="minnie.acer.com.tw"
 MICKEY="mickey.acer.com.tw"
 MINNIE_WHITE_LIST=["acer/dandelion", "acer/conga", "acer/timbero", "acer/bongo", "acer/bolero", "acer/asf", "acer/partyshot", "acer/icontrol", "acer/iot/openwrt"]
-MICKEY_WHITE_LIST=[]
-HIPCHAT_AUTH_TOKEN=""
-HIPCHAT_ROOM_ID=""
-REDMINE_API_KEY=""
+MICKEY_WHITE_LIST=["acer/apps/CloudPBX"]
+HIPCHAT_AUTH_TOKEN="244a74652574b9bd0e43b98ecd1ff9"
+HIPCHAT_ROOM_ID="324703"
+REDMINE_API_KEY="003400126b0ef372e294c2916515dcde2cf5b2c3"
 
 gb_minnie = GerritBot(MINNIE, MINNIE_WHITE_LIST, HIPCHAT_AUTH_TOKEN, HIPCHAT_ROOM_ID, REDMINE_API_KEY)
 gb_mickey = GerritBot(MICKEY, MICKEY_WHITE_LIST, HIPCHAT_AUTH_TOKEN, HIPCHAT_ROOM_ID, REDMINE_API_KEY)
